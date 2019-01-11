5a1fcae5dda5c42110bec597c604e0d56b84a52f
viveknigam3003 <viveknigam.nigam3@gmail.com>
2019-01-11 18:13:39 +0530

    Files Updated

--- macbuntu.sh
+++
@@ -1,4 +0,0 @@
-#Installing Gnome Shell
-sudo apt-get install gnome-shell
-sudo apt-get update
-sudo apt-get install gnome-tweak-tool
---
+++ unzipfiles.sh
@@ -0,0 +1,4 @@
+sudo apt-get install unzip
+unzip macOS Cursor Set.zip
+unzip macOS-3.2.zip
+unzip OS-X-Mavericks-master.zip
